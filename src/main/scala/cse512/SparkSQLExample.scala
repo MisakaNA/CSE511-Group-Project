7e4d343cfd78f13b95eaf2be11a6b2d01eb5cc87
MisakaNA <sunyumeng420@gmail.com>
2021-04-12 17:33:00 -0700

    update

--- src/main/scala/cse512/SparkSQLExample.scala
+++ src/main/scala/cse512/SparkSQLExample.scala
@@ -93,10 +93,15 @@ object SparkSQLExample {
     val pointY = valueArray(1).asInstanceOf[Double]
 
     valueArray = queryRectangle.split(',')
-    val rectX1 = valueArray(0).asInstanceOf[Double]
-    val rectY1 = valueArray(1).asInstanceOf[Double]
-    val rectX2 = valueArray(2).asInstanceOf[Double]
-    val rectY2 = valueArray(3).asInstanceOf[Double]
+    //val rectX1 = valueArray(0).asInstanceOf[Double]
+    //val rectY1 = valueArray(1).asInstanceOf[Double]
+    //val rectX2 = valueArray(2).asInstanceOf[Double]
+    //val rectY2 = valueArray(3).asInstanceOf[Double]
+
+    val rectX1 = math.min(valueArray(0).asInstanceOf[Double], valueArray(2).asInstanceOf[Double])
+    val rectY1 = math.min(valueArray(1).asInstanceOf[Double], valueArray(3).asInstanceOf[Double])
+    val rectX2 = math.max(valueArray(0).asInstanceOf[Double], valueArray(2).asInstanceOf[Double])
+    val rectY2 = math.min(valueArray(1).asInstanceOf[Double], valueArray(3).asInstanceOf[Double])
 
     pointX >= rectX1 && pointX <= rectX2 && pointY >= rectY1 && pointY <= rectY2
   }
